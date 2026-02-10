0aaf4ae2060baf228892c6c5aa79ea8d10f9f981
Binula Dimantha <104006275+Bin4yi@users.noreply.github.com>
2026-02-10 10:19:51 +0530

    Add new constants for delegation and actor claims

--- component/org.wso2.carbon.identity.oauth2.grant.token.exchange/src/main/java/org/wso2/carbon/identity/oauth2/grant/token/exchange/Constants.java
+++ component/org.wso2.carbon.identity.oauth2.grant.token.exchange/src/main/java/org/wso2/carbon/identity/oauth2/grant/token/exchange/Constants.java
@@ -65,6 +65,10 @@ public class Constants {
         public static final String SUB = "sub";
         public static final String AZP = "azp";
         public static final String SCOPE = "scope";
+        public static final String IS_DELEGATION_REQUEST = "IS_DELEGATION_REQUEST";
+        public static final String ACTOR_SUBJECT = "ACTOR_SUBJECT";
+        public static final String ACTOR_AZP = "ACTOR_AZP";
+        public static final String EXISTING_ACT_CLAIM = "EXISTING_ACT_CLAIM";
 
     }
 
