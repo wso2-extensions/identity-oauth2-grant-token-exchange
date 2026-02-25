5ae2c1c480c0b43f2ad75f61480f5acc0a1fc0c0
Binula Dimantha <104006275+Bin4yi@users.noreply.github.com>
2026-02-25 12:28:22 +0530

    Update component/org.wso2.carbon.identity.oauth2.grant.token.exchange/src/main/java/org/wso2/carbon/identity/oauth2/grant/token/exchange/TokenExchangeGrantHandler.java
    Co-authored-by: Pavindu Lakshan <pavindulakshan@gmail.com>

--- component/org.wso2.carbon.identity.oauth2.grant.token.exchange/src/main/java/org/wso2/carbon/identity/oauth2/grant/token/exchange/TokenExchangeGrantHandler.java
+++ component/org.wso2.carbon.identity.oauth2.grant.token.exchange/src/main/java/org/wso2/carbon/identity/oauth2/grant/token/exchange/TokenExchangeGrantHandler.java
@@ -734,10 +734,6 @@ public class TokenExchangeGrantHandler extends AbstractAuthorizationGrantHandler
         String actorTokenSubject = resolveSubject(claimsSet);
         validateMandatoryClaims(claimsSet, actorTokenSubject);
 
-        // NOTE: For delegation, we skip the impersonator check since there's no may_act
-        // claim
-        // in the subject token. The actor is simply delegating on behalf of the
-        // subject.
 
         String jwtIssuer = claimsSet.getIssuer();
         IdentityProvider identityProvider = getIdentityProvider(tokReqMsgCtx, jwtIssuer, tenantDomain);
